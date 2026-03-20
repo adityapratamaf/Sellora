439af024fe1db72791288dd9adfa78677f25e802
Aditya Pratama Febriono <adityapeef18@gmail.com>
2026-03-20 16:15:45 +0700

    delete before migrate reload

--- Api/Program.cs
+++ Api/Program.cs
@@ -44,27 +44,19 @@ if (app.Environment.IsDevelopment())
 using (var scope = app.Services.CreateScope())
 {
     var db = scope.ServiceProvider.GetRequiredService<AppDbContextType>();
+
+    // hapus database lama
+    await db.Database.EnsureDeletedAsync();
+
+    // buat ulang database sesuai migrasi terbaru
     await db.Database.MigrateAsync();
+
+    // jalankan seeder
     await UserSeeder.SeedAsync(db);
     await CategorySeeder.SeedAsync(db);
     await ProductSeeder.SeedAsync(db);
     await PaymentSeeder.SeedAsync(db);
 }
-
-// using (var scope = app.Services.CreateScope())
-// {
-//     var db = scope.ServiceProvider.GetRequiredService<AppDbContextType>();
-
-//     await db.Database.MigrateAsync();
-
-//     if (!db.Users.Any())
-//     {
-//         await UserSeeder.SeedAsync(db);
-//         await CategorySeeder.SeedAsync(db);
-//         await ProductSeeder.SeedAsync(db);
-//         await PaymentSeeder.SeedAsync(db);
-//     }
-// }
 
 app.UseStaticFiles();
 
